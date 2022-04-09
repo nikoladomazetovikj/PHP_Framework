980c5a177fa7d02032dfce8bf666c02b11540c73
nikola_domazetovik <nikolad150@gmail.com>
2022-04-09 14:19:05 +0200

    router created

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use app\core\Application;
+
+$app = new Application();
+
+$app->router->get('/', function () {
+
+    return "Hello world!";
+});
+
+$app->router->get('/aboutme', function () {
+
+    return "I am Nikola!";
+});
+
+$app->router->get('/contact', function () {
+
+    return "This is my contact";
+});
+
+
+
+$app->run();
