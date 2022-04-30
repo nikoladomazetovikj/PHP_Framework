c03b52f5d0ac9404b2964ece278e5f8250370ced
nikola_domazetovik <nikolad150@gmail.com>
2022-04-30 02:00:37 +0200

    migration executable

--- core/Database.php
+++ core/Database.php
@@ -17,4 +17,70 @@ class Database
         $this->pdo = new PDO($dsn, $user, $password);
         $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
+
+    public function applyMigrations()
+    {
+        $this->createMigrationsTable();
+        $appliedMigrations =  $this->getAppliedMigrations();
+
+        $newMigrations = [];
+        $files = scandir(Application::$rootDIR . '/migrations');
+
+        $toApplyMigrations =  array_diff($files, $appliedMigrations);
+
+        foreach ($toApplyMigrations as $migration) {
+
+            if ($migration === '.' || $migration === '..') {
+
+                continue;
+            }
+
+            require_once Application::$rootDIR . '/migrations/' . $migration;
+
+            $className = pathinfo($migration, PATHINFO_FILENAME);
+
+            $instance = new $className;
+            $this->log('Migrating: ' . $migration);
+            $instance->up();
+            $this->log($migration . " migrated ");
+
+            $newMigrations[] = $migration;
+        }
+
+        if (!empty($newMigrations)) {
+            $this->saveMigrations($newMigrations);
+        } else {
+            $this->log('Nothing to migrate');
+        }
+    }
+
+    public function createMigrationsTable()
+    {
+        $this->pdo->exec('CREATE TABLE IF NOT EXISTS migrations (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            migration VARCHAR(255),
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP       
+        );');
+    }
+
+    public function getAppliedMigrations()
+    {
+        $statement = $this->pdo->prepare('SELECT migration FROM migrations');
+        $statement->execute();
+
+        return $statement->fetchAll(PDO::FETCH_COLUMN);
+    }
+
+    public function saveMigrations(array $migrations)
+    {
+        $str = implode(",", array_map(fn ($m) => "('$m')", $migrations));
+        $statement = $this->pdo->prepare("INSERT INTO migrations (migration) VALUE ($str)");
+
+        $statement->execute();
+    }
+
+    protected function log($message)
+    {
+        echo "[" . date('Y-m-d H:i:s') . "] " . $message . PHP_EOL;
+    }
 }
--- migrations/m0001_initial.php
+++ migrations/m0001_initial.php
@@ -1,15 +1,28 @@
 <?php
 
+use app\core\Application;
 
 class m0001_initial
 {
     public function up()
     {
-        echo "up";
+        $db = Application::$app->db;
+        $SQL = "CREATE TABLE users (
+                id INT AUTO_INCREMENT PRIMARY KEY ,
+                firstname VARCHAR(255) NOT NULL ,
+                lastname VARCHAR(255) NOT NULL ,
+                email VARCHAR(255) NOT NULL ,
+                status TINYINT NOT NULL,
+                created_at TIMESTAMP DEFAULT  CURRENT_TIMESTAMP
+        )";
+        $db->pdo->exec($SQL);
     }
 
     public function down()
     {
-        echo 'down';
+
+        $db = Application::$app->db;
+        $SQL = "DROP TABLE users;";
+        $db->pdo->exec($SQL);
     }
 }
